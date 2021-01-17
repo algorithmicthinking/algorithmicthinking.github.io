584ad780e4dcb8372260537ce77039f469128bd3
Rahul Shah <rsha256@berkeley.edu>
2021-01-16 17:19:46 -0800

    merge

--- src/data/Staff.js
+++ src/data/Staff.js
@@ -1,33 +1,107 @@
 import Temp from "../img/logo.png";
+import Ahan from "../headshots/ahan_sabharwal.jpg";
+import Michael from "../headshots/michaelwang23.jpg";
+import Ankit from "../headshots/ankit_agarwal.jpg";
+import David from "../headshots/michaelwang23.jpg";
 
 export const facilitators = [
-  { name: "Michael Jayasuriya", email: "mjayasur", bio: "Hi,", image: Temp },
-  { name: "Kathy Wu", email: "kathywu", bio: "Hi!", image: Temp },
+  { name: "Michael Jayasuriya", email: "mjayasur", bio: "Hi,", image: Temp 
+  },
+  { name: "Kathy Wu", email: "kathywu", bio: "Hi!", image: Temp 
+  },
 ];
 
 export const teachingAssistants = [
   {
     name: "Ahan Sabharwal",
     email: "ahan.sabharwal",
-    bio:
-      "Hi everyone! I’m a junior studying Data Science and CS. I’m interested in systems, ML infra, and algorithms. Super excited to TA the course and help everyone with interview prep!",
-    image: Temp,
+    bio: "Hi everyone! I’m a junior studying Data Science and CS. I’m interested in systems, ML infra, and algorithms. Super excited to TA the course and help everyone with interview prep!",
+    image: Ahan,
   },
-  { name: "Michael Wang", email: "michaelwang23", bio: "Hi", image: Temp },
-  { name: "Ankit Agarwal", email: "aagarwal_601", bio: "Hi", image: Temp },
-  { name: "Megan Zhu", email: "", bio: "Hi", image: Temp },
-  { name: "David Zhu", email: "zidongzhu", bio: "Hi", image: Temp },
-  { name: "Kexin (Cassie) Shu", email: "kexinshu", bio: "Hi", image: Temp },
-  { name: "Leo Kao", email: "leok", bio: "Hi", image: Temp },
-  { name: "Ian Chang", email: "iac", bio: "Hi", image: Temp },
-  { name: "Kevin Zhu", email: "zhu502846", bio: "Hi", image: Temp },
-  { name: "Richard Zhu", email: "rszhu", bio: "Hi", image: Temp },
-  { name: "Rahul Shah", email: "rsha256", bio: "Hi", image: Temp },
-  { name: "Samarth Goel", email: "sgoel9", bio: "Hi", image: Temp },
-  { name: "Priyam Mohanty", email: "", bio: "Hi", image: Temp },
-  { name: "Luke Liu", email: "lukeliu", bio: "Hi", image: Temp },
-  { name: "Artem Tkachuk", email: "", bio: "Hi", image: Temp },
-  { name: "Harris Shadmany", email: "harrisshadmany", bio: "Hi", image: Temp },
-  { name: "Julian Meyn", email: "meyn", bio: "Hi", image: Temp },
-  { name: "Jessica Yu", email: "jessica.yu", bio: "Hi", image: Temp },
+  { 
+    name: "Michael Wang", 
+    email: "michaelwang23", 
+    bio: "Hi, I'm Michael and I'm a 2nd year CS major. Looking forward to a great semester!", 
+    image: Michael 
+  },
+  { name: "Ankit Agarwal", 
+    email: "aagarwal_601", 
+    bio: "Hi", 
+    image: Ankit 
+  },
+  { name: "Megan Zhu", 
+    email: "", 
+    bio: "Hi", 
+    image: Temp 
+  },
+  { name: "David Zhu", 
+    email: "zidongzhu", 
+    bio: "Hi", 
+    image: Temp 
+  },
+  { name: "Kexin (Cassie) Shu", 
+    email: "kexinshu", 
+    bio: "Hi", 
+    image: Temp 
+  },
+  { name: "Leo Kao", 
+    email: "leok", 
+    bio: "Hi", 
+    image: Temp 
+  },
+  { name: "Ian Chang", 
+    email: "iac", 
+    bio: "Hi", 
+    image: Temp 
+  },
+  { name: "Kevin Zhu", 
+    email: "zhu502846", 
+    bio: "Hi", 
+    image: Temp 
+  },
+  { name: "Richard Zhu", 
+    email: "rszhu", 
+    bio: "Hi", 
+    image: Temp 
+  },
+  { name: "Rahul Shah", 
+    email: "rsha256", 
+    bio: "Hi", 
+    image: Temp 
+  },
+  { name: "Samarth Goel", 
+    email: "sgoel9", 
+    bio: "Hi", 
+    image: Temp 
+  },
+  { name: "Priyam Mohanty", 
+    email: "priyam.mohanty", 
+    bio: "Hey everyone, I'm a senior majoring in CS and Public Health. Happy to help with anything ranging from algorithms to recruiting help.", 
+    image: Temp 
+  },
+  { name: "Luke Liu", 
+    email: "lukeliu", 
+    bio: "Hi", 
+    image: Temp 
+  },
+  { name: "Artem Tkachuk", 
+    email: "atkachuk", 
+    bio: "Hi", 
+    image: Temp 
+  },
+  { name: "Harris Shadmany", 
+    email: "harrisshadmany", 
+    bio: "Hi", 
+    image: Temp 
+  },
+  { name: "Julian Meyn", 
+    email: "meyn", 
+    bio: "Hi", 
+    image: Temp 
+  },
+  { name: "Jessica Yu", 
+    email: "jessica.yu", 
+    bio: "Hi", 
+    image: Temp 
+  },
 ];
