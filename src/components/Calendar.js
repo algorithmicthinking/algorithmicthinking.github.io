f08cc4e1d0a6b86472826105af0799d847e17fea
Rahul Shah <rsha256@berkeley.edu>
2021-02-06 00:01:47 -0800

    hide cal on mobile

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,3 +1,4 @@
+import "../css/cal.css";
 import React, { Component, useState } from 'react';
 
 import { lightTheme, darkTheme } from './Theme';
@@ -12,10 +13,10 @@ const Calendar = () => {
         return <div />
     };
 
-    const iframe = '<iframe\n' +
+    const iframe = '<div class="responsiveCal"><iframe\n' +
         '            src="https://calendar.google.com/calendar/embed?src=c_9acra227ucphcf4eq6m7theu3o%40group.calendar.google.com&ctz=America%2FLos_Angeles "\n' +
         '            style="border: 0" width="1080" height="800" frameBorder="0" scrolling="no">\n' +
-        '        </iframe>'
+        '        </iframe></div>'
 
     function Iframe(props) {
         return (<div dangerouslySetInnerHTML={ {__html:  props.iframe?props.iframe:""}} />);
