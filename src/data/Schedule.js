232b9160f53590fe0af7d8245ee28bb3e71c6ebc
Samarth Goel <samarthgoel@Samarths-MacBook-Air.local>
2021-09-03 17:26:05 -0700

    update hw

--- src/data/Schedule.js
+++ src/data/Schedule.js
@@ -5,60 +5,82 @@ export const schedule = [
     topic: "Introduction and a Brief Overview of the Interview Process",
     slides: "",
     video: "",
-    problems: [],
+    problems: [
+      {
+        name: "Product of array except self",
+        link: "https://leetcode.com/problems/product-of-array-except-self/",
+      },
+      {
+        name: "Merge Intervals",
+        link: "https://leetcode.com/problems/merge-intervals/",
+      },
+      {
+        name: "Best time to buy and sell stock",
+        link: "https://leetcode.com/problems/best-time-to-buy-and-sell-stock/",
+      },
+    ],
     sectionName: "",
     sectionProblem: "",
-    homework: "Get a LeetCode Account",
-    due: "",
+    homework: "Homework 0",
+    due: "09/14 Tu",
   },
   // Week 1
   {
     date: "09/14 Tu",
-    topic: "Basic Data Structures and Problem Solving",
+    topic: "Hashmaps",
     slides: "",
     video: "",
     sectionName: "",
     sectionProblem: "",
     sectionInfo: "",
     problems: [
-      // {
-      //   name: "Two Sum",
-      //   link: "https://leetcode.com/problems/two-sum/"
-      // },
-      // {
-      //   name: "Contains duplicate",
-      //   link: "https://leetcode.com/problems/contains-duplicate/"
-      // },{
-      //   name: "[Solution] Contains duplicate",
-      //   link: "https://www.youtube.com/watch?v=u0EODaL6wQE&list=PLqscWxznaF7fjTcZ5nFj5oe7_bs6fMLzj"
-      // },
-      // {
-      //   name: "Best time to buy and sell stock",
-      //   link: "https://leetcode.com/problems/best-time-to-buy-and-sell-stock/"
-      // },
-      // {
-      //   name: "Merge Intervals",
-      //   link: "https://leetcode.com/problems/merge-intervals/"
-      // },
-      // {
-      //   name: "Product of array except self",
-      //   link: "https://leetcode.com/problems/product-of-array-except-self/"
-      // },
-      // {
-      //   name: "[Solution] Product of array except self",
-      //   link: "https://www.youtube.com/watch?v=wNi-xFJ9pOk&list=PLqscWxznaF7fjTcZ5nFj5oe7_bs6fMLzj&index=4"
-      // },
-      // {
-      //   name: "[Solution] Valid Anagram",
-      //   link: "https://www.youtube.com/watch?v=9dEAZFF4CRc&list=PLqscWxznaF7fjTcZ5nFj5oe7_bs6fMLzj&index=2"
-      // },
+      {
+        name: "Two Sum",
+        link: "https://leetcode.com/problems/two-sum/",
+      },
+      {
+        name: "Contains duplicate ii",
+        link: "https://leetcode.com/problems/contains-duplicate-ii/",
+      },
+      {
+        name: "LRU Cache",
+        link: "https://leetcode.com/problems/lru-cache/",
+      },
     ],
-    homework: "",
-    due: "",
+    homework: "Homework 1",
+    due: "09/21 Tu",
   },
   // Week 2
   {
     date: "09/21 Tu",
+    topic: "Strings",
+    slides: "",
+    video: "",
+    sectionName: "",
+    sectionProblem: "",
+    sectionInfo: "",
+    extraPracticeName: "",
+    extraPracticeProblem: "",
+    problems: [
+      {
+        name: "First Unique Character in a String",
+        link: "https://leetcode.com/problems/first-unique-character-in-a-string/",
+      },
+      {
+        name: "Add Strings",
+        link: "https://leetcode.com/problems/add-strings/",
+      },
+      {
+        name: "Shortest Palindrome",
+        link: "https://leetcode.com/problems/shortest-palindrome/",
+      },
+    ],
+    homework: "Homework 2",
+    due: "09/28 Tu",
+  },
+  // Week 3
+  {
+    date: "09/28 Tu",
     topic: "Linked Lists",
     slides: "",
     video: "",
@@ -68,244 +90,125 @@ export const schedule = [
     extraPracticeName: "",
     extraPracticeProblem: "",
     problems: [
-      // {
-      //   name: "Reverse a Linked List",
-      //   link: "https://leetcode.com/problems/reverse-linked-list/",
-      // },
-      // {
-      //   name: "Reverse Linked List ii",
-      //   link: "https://leetcode.com/problems/reverse-linked-list-ii/",
-      // },
-      // {
-      //   name: "[Solution] Reverse Linked List ii",
-      //   link: "https://www.youtube.com/watch?v=ZiZqqvqEnrs",
-      // },
-      // {
-      //   name: "Flatten a multilevel linkedlist",
-      //   link: "https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/",
-      // },
-      // {
-      //   name: "[Solution] Flatten a multilevel linkedlist",
-      //   link: "https://youtu.be/Ly1EEJWMLfk",
-      // },
-      // {
-      //   name: "Remove nth node from end",
-      //   link: "https://leetcode.com/problems/remove-nth-node-from-end-of-list/",
-      // },
-      // {
-      //   name: "[Solution] Remove nth node from end",
-      //   link: "https://youtu.be/LC0y6w4amhg",
-      // },
-      // {
-      //   name: "Merge 2 sorted lists",
-      //   link: "https://leetcode.com/problems/merge-two-sorted-lists/",
-      // },
+      {
+        name: "Merge Two Sorted Lists",
+        link: "https://leetcode.com/problems/merge-two-sorted-lists/",
+      },
+      {
+        name: "Detect Cycle in a Linked List",
+        link: "https://leetcode.com/problems/linked-list-cycle/",
+      },
+      {
+        name: "Reorder List",
+        link: "https://leetcode.com/problems/reorder-list/",
+      },
     ],
-    homework: "",
-    due: "",
-  },
-  // Week 3
-  {
-    date: "09/28 Tu",
-    topic: "Binary Search Problems",
-    slides: "",
-    video: "",
-    sectionName: "",
-    sectionProblem: "",
-    sectionInfo: "",
-    extraPracticeName: "",
-    extraPracticeProblem: "",
-    problems: [
-      // {
-      //   name: "First & last pos in sorted arr",
-      //   link: "https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/",
-      // },
-      // {
-      //   name: "[Solution] First & last pos in sorted arr",
-      //   link: "https://youtu.be/0yktIGutogE",
-      // },
-      // {
-      //   name: "Search Insert Position",
-      //   link: "https://leetcode.com/problems/search-insert-position/",
-      // },
-      // {
-      //   name: "[Solution] Search Insert Position",
-      //   link: "https://youtu.be/SQfFsQbZUfs",
-      // },
-      // {
-      //   name: "Search in rotated sorted arr",
-      //   link: "https://leetcode.com/problems/search-in-rotated-sorted-array/",
-      // },
-      // {
-      //   name: "[Solution] Search in rotated sorted arr",
-      //   link: "https://youtu.be/19thO-YJcN4",
-      // },
-      // {
-      //   name: "Random pick with weight",
-      //   link: "https://leetcode.com/problems/random-pick-with-weight/",
-      // },
-      // {
-      //   name: "[Solution] Random pick with weight",
-      //   link: "https://youtu.be/7UY2XbFcW5A",
-      // },
-      // {
-      //   name: "Find smallest divisor w/ threshold",
-      //   link: "https://leetcode.com/problems/find-the-smallest-divisor-given-a-threshold/",
-      // },
-      // {
-      //   name: "[Solution] Find smallest divisor w/ threshold",
-      //   link: "https://youtu.be/F50a7MiIkrE",
-      // },
-    ],
-    homework: "",
-    due: "",
+    homework: "Homework 3",
+    due: "10/05 Tu",
   },
 
   // Week 4
   {
     date: "10/05 Tu",
-    topic: "Intermediate Data Structures: Stacks, Queues, and Heaps",
+    topic: "Binary Search",
     slides: "",
     video: "",
     sectionName: "",
     sectionProblem: "",
     // sectionInfo: "",
     problems: [
-      // {
-      //   name: "Valid Parentheses",
-      //   link: "https://leetcode.com/problems/valid-parentheses/",
-      // },
-      // {
-      //   name: "[Solution] Valid Parentheses",
-      //   link: "https://youtu.be/8pI2EWjAXmM",
-      // },
-      // {
-      //   name: "Minimum to Remove to Make Valid",
-      //   link: "https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/",
-      // },
-      // {
-      //   name: "K Closest Points to Origin",
-      //   link: "https://leetcode.com/problems/k-closest-points-to-origin/",
-      // },
-      // {
-      //   name: "LRU Cache",
-      //   link: "https://leetcode.com/problems/lru-cache/",
-      // },
-      // {
-      //   name: "Task Scheduler",
-      //   link: "https://leetcode.com/problems/task-scheduler/",
-      // },
+      {
+        name: "Search Insert Position",
+        link: "https://leetcode.com/problems/search-insert-position/",
+      },
+      {
+        name: "Find First and Last Position of Element in Sorted Array",
+        link: "https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/",
+      },
+      {
+        name: "Search in Rotated Sorted Array",
+        link: "https://leetcode.com/problems/search-in-rotated-sorted-array/",
+      },
     ],
-    homework: "",
-    due: "",
+    homework: "Homework 4",
+    due: "10/12 Tu",
   },
   // Week 5
   {
     date: "10/12 Tu",
-    topic: "Object Oriented Programming",
+    topic: "Intermediate Data Structures: Stacks, Queues, and Heaps",
     sectionName: "",
     slides: "",
     video: "",
     sectionProblem: "",
     problems: [
-      // {
-      //   name: "Min Stack",
-      //   link: "https://leetcode.com/problems/min-stack/",
-      // },
-      // {
-      //   name: "[Solution] Min Stack",
-      //   link: "https://www.youtube.com/watch?v=3dN1dwbufDg",
-      // },
-      // {
-      //   name: "Design Underground Subway System",
-      //   link: "https://leetcode.com/problems/design-underground-system/",
-      // },
-      // {
-      //   name: "Design Underground Subway System",
-      //   link: "https://www.youtube.com/watch?v=Vom-eJy6hWs",
-      // },
-      // {
-      //   name: "Design Twitter",
-      //   link: "https://leetcode.com/problems/design-twitter/",
-      // },
-      // {
-      //   name: "Design Hashset",
-      //   link: "https://leetcode.com/problems/design-hashset/",
-      // },
-      // {
-      //   name: "Encode and Decode TinyURL",
-      //   link: "https://leetcode.com/problems/encode-and-decode-tinyurl/",
-      // },
+      {
+        name: "Valid Parentheses",
+        link: "https://leetcode.com/problems/valid-parentheses/",
+      },
+      {
+        name: "Top K Frequent Elements",
+        link: "https://leetcode.com/problems/top-k-frequent-elements/",
+      },
+      {
+        name: "Sliding Window Maximum",
+        link: "https://leetcode.com/problems/sliding-window-maximum/",
+      },
     ],
-    homework: "",
-    due: "",
+    homework: "Homework 5",
+    due: "10/19 Tu",
   },
   // Week 6
   {
     date: "10/19 Tu",
-    topic: "Two Pointers/Sliding Window",
+    topic: "Design Questions",
     extra: "",
     slides: "",
     video: "",
     sectionName: "",
     sectionProblem: "",
-    interviewHelp: [
-      // {
-      //   name: "Problem 1",
-      //   link: "https://docs.google.com/document/d/15tl8bdDm9IdipFOdXIsy7puLlo3VcoyCxN3wc-6tsIE/edit#",
-      //   solution: "https://www.youtube.com/watch?v=KVMffdODBJI",
-      // },
-      // {
-      //   name: "Problem 2",
-      //   link: "https://docs.google.com/document/d/1oHgvt76_sAM6z5khjQyN7ek-iDupWHRytumF3jIcWbQ/edit?usp=sharing",
-      //   solution: "https://www.youtube.com/watch?v=pObhgBa-lT4",
-      // },
-      // {
-      //   name: "Example Mock Interview",
-      //   link: "https://youtu.be/SxP6BeaG3cw",
-      // },
-    ],
+    interviewHelp: ["", "", ""],
 
     problems: [
-      // {
-      //   name: "Longest Substring without Repeating Characters",
-      //   link: "https://leetcode.com/problems/longest-substring-without-repeating-characters/",
-      // },
-      // {
-      //   name: "Maximum Points Obtain from Cards",
-      //   link: "https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/",
-      // },
-      // {
-      //   name: "Maximum Continuous Ones",
-      //   link: "https://leetcode.com/problems/max-consecutive-ones-iii/",
-      // },
-      // {
-      //   name: "Permutation in String",
-      //   link: "https://leetcode.com/problems/permutation-in-string/",
-      // },
-      // {
-      //   name: "Minimum Window Substring",
-      //   link: "https://leetcode.com/problems/minimum-window-substring/",
-      // },
+      {
+        name: "Min Stack",
+        link: "https://leetcode.com/problems/min-stack/",
+      },
+      {
+        name: "Design Hashset",
+        link: "https://leetcode.com/problems/design-hashset/",
+      },
+      {
+        name: "Design Underground Subway System",
+        link: "https://leetcode.com/problems/design-underground-system/",
+      },
     ],
 
-    homework: "",
-    due: "",
+    homework: "Homework 6",
+    due: "10/26 Tu",
   },
   // Week 7
   {
     date: "10/26 Tu",
-    topic: "TBD",
+    topic: "Sliding Window",
     // sectionName: "Add Two Numbers",
     // sectionProblem: "https://leetcode.com/problems/add-two-numbers/",
     problems: [
       {
-        name: "",
-        link: "",
+        name: "Maximum Continuous Ones",
+        link: "https://leetcode.com/problems/max-consecutive-ones-iii/",
+      },
+      {
+        name: "Permutation in String",
+        link: "https://leetcode.com/problems/permutation-in-string/",
+      },
+      {
+        name: "Minimum Window Substring",
+        link: "https://leetcode.com/problems/minimum-window-substring/",
       },
     ],
-    homework: "",
-    // due: "",
+    homework: "Homework 7",
+    due: "11/02 Tu",
   },
   // Week 8
   {
@@ -316,29 +219,21 @@ export const schedule = [
     sectionProblem: "",
     slides: "",
     problems: [
-      // {
-      //   name: "Same Tree",
-      //   link: "https://leetcode.com/problems/same-tree/",
-      // },
-      // {
-      //   name: "Kth Smallest Element in a BST",
-      //   link: "https://leetcode.com/problems/kth-smallest-element-in-a-bst/",
-      // },
-      // {
-      //   name: "Maximum Depth of a Binary Tree",
-      //   link: "https://leetcode.com/problems/maximum-depth-of-binary-tree/",
-      // },
-      // {
-      //   name: "Diameter of a Binary Tree",
-      //   link: "https://leetcode.com/problems/diameter-of-binary-tree/",
-      // },
-      // {
-      //   name: "Right Side Binary Tree",
-      //   link: "https://leetcode.com/problems/binary-tree-right-side-view/",
-      // },
+      {
+        name: "Same Tree",
+        link: "https://leetcode.com/problems/same-tree/",
+      },
+      {
+        name: "Kth Smallest Element in a BST",
+        link: "https://leetcode.com/problems/kth-smallest-element-in-a-bst/",
+      },
+      {
+        name: "Right Side Binary Tree",
+        link: "https://leetcode.com/problems/binary-tree-right-side-view/",
+      },
     ],
-    homework: "",
-    due: "",
+    homework: "Homework 8",
+    due: "11/09 Tu",
   },
   // Week 9
   {
@@ -349,25 +244,21 @@ export const schedule = [
     slides: "",
     video: "",
     problems: [
-      // {
-      //   name: "Course Schedule",
-      //   link: "https://leetcode.com/problems/course-schedule/",
-      // },
-      // {
-      //   name: "Word Search",
-      //   link: "https://leetcode.com/problems/word-search/",
-      // },
-      // {
-      //   name: "Number of Islands",
-      //   link: "https://leetcode.com/problems/number-of-islands/",
-      // },
-      // {
-      //   name: "Rotting Oranges",
-      //   link: "https://leetcode.com/problems/rotting-oranges/",
-      // },
+      {
+        name: "Number of Islands",
+        link: "https://leetcode.com/problems/number-of-islands/",
+      },
+      {
+        name: "Course Schedule",
+        link: "https://leetcode.com/problems/course-schedule/",
+      },
+      {
+        name: "Swim in Rising Water",
+        link: "https://leetcode.com/problems/swim-in-rising-water/",
+      },
     ],
-    homework: "",
-    due: "",
+    homework: "Homework 9",
+    due: "11/16 Tu",
   },
   // Week 10
   {
@@ -380,29 +271,21 @@ export const schedule = [
     extraPracticeProblem: "",
     video: "",
     problems: [
-      // {
-      //   name: "Decode ways",
-      //   link: "https://leetcode.com/problems/decode-ways/",
-      // },
-      // {
-      //   name: "Maximum subarray sum",
-      //   link: "https://leetcode.com/problems/maximum-subarray/",
-      // },
-      // {
-      //   name: "Longest Palindromic Substring",
-      //   link: "https://leetcode.com/problems/longest-palindromic-substring/",
-      // },
-      // {
-      //   name: "Partition Equal Subset Sum",
-      //   link: "https://leetcode.com/problems/partition-equal-subset-sum/",
-      // },
-      // {
-      //   name: "Longest common subsequence",
-      //   link: "https://leetcode.com/problems/longest-common-subsequence/",
-      // },
+      {
+        name: "Maximum Subarray Sum",
+        link: "https://leetcode.com/problems/maximum-subarray/",
+      },
+      {
+        name: "Decode Ways",
+        link: "https://leetcode.com/problems/decode-ways/",
+      },
+      {
+        name: "Longest Palindromic Substring",
+        link: "https://leetcode.com/problems/longest-palindromic-substring/",
+      },
     ],
-    homework: "",
-    due: "",
+    homework: "Homework 10",
+    due: "11/23 Tu",
   },
   // Week 11
   {
@@ -413,21 +296,21 @@ export const schedule = [
     sectionProblem: "",
     video: "",
     problems: [
-      // {
-      //   name: "Gas Station",
-      //   link: "https://leetcode.com/problems/gas-station/",
-      // },
-      // {
-      //   name: "Task Scheduler",
-      //   link: "https://leetcode.com/problems/task-scheduler/",
-      // },
-      // {
-      //   name: "Boats to Save People",
-      //   link: "https://leetcode.com/problems/boats-to-save-people/",
-      // },
+      {
+        name: "Task Scheduler",
+        link: "https://leetcode.com/problems/task-scheduler/",
+      },
+      {
+        name: "Gas Station",
+        link: "https://leetcode.com/problems/gas-station/",
+      },
+      {
+        name: "Boats to Save People",
+        link: "https://leetcode.com/problems/boats-to-save-people/",
+      },
     ],
-    homework: "",
-    due: "",
+    homework: "Homework 11",
+    due: "11/30 Tu",
   },
   // Week 12
   {
